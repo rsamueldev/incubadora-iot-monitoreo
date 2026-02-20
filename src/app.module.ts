ce82d2e4cce229704578882ef19bc1545f36fcf5
gonzaloap1 <gonzaloperezmontengro@gmail.com>
2026-02-19 23:48:51 -0400

    Instalacion de mapped-types y correccion de errores de compilacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DevicesModule } from './devices/devices.module'; // Agrega esta línea
+import { ReadingsModule } from './readings/readings.module'; // Agrega esta línea
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DevicesModule, // Agrégalo aquí
+    ReadingsModule, // Agrégalo aquí
+  ],
 })
 export class AppModule {}
